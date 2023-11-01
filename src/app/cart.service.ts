ed4170cf108c99de11a61da76e8bf98fc4397886
janpi242 <piotr@code-q.pl>
2023-11-01 19:34:44 +0100

    implement shipping prices and cart

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,12 +1,17 @@
-import { Product } from './products';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Product } from './products';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  
   items: Product[] = [];
-  constructor() { }
+  
+  constructor(
+    private http: HttpClient
+  ) {}
 
   addToCart(product: Product) {
     this.items.push(product);
@@ -19,5 +24,9 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
   }
 }
